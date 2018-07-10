965ea4dae2ecbf850384ae8be3598bca76cf9da8
dunkyfool <dunkyfool0113@gmail.com>
2018-07-10 08:43:34 +0800

    add roadmap

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
-# job list
+# job list (list each job's gm week)
 jobs = [5,4,3]
 jobs.sort()
 
 # define capability
-startweek = 2
-queue = 2
-duration = 2
+startweek = 2      # calculate week
+queue = 2          # the number of handling jobs per week
+duration = 2       # how long to finish the job
 
 # counter
-ctr = 0
-total_diff = 0
+ctr = 0            # counter for queue
+idx = 0            # index of job
+total_diff = 0     # index
+roadmap = []
+thisweek = []
 
 for gm in jobs:
     diff = gm - (startweek + duration)
-    if diff < 0:
-        print gm
+    if diff < 0:   # highlight which job cannot finish before GM
+      print 'This job:', idx, ' will not finish on time!'
     total_diff += diff
+    thisweek += [gm]
     ctr += 1
+    idx += 1
     if ctr % queue == 0:
         ctr = 0
         startweek += 1
+        roadmap += [thisweek]
+        thisweek = []
 
-print total_diff
+if len(thisweek) != 0:
+    roadmap += [thisweek]
+    thisweek = []
+
+print 'Total remain time:', total_diff
+print 'Roadmap:'; print roadmap
